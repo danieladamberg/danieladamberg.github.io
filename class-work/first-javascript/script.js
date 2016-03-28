126e1eb313e1ca209ca6000e08c8d7ef1afd8c9c
danieladamberg <danieladamberg@gmail.com>
2016-03-28 18:38:39 -0400

    Startup-Matchmaker HW

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,29 @@
+
+
+document.write('writing to the document, like a boss');
+
+//alert('this is an alert. this is annoying');
+
+// var num = 1000000
+
+// var num2 = 10*10
+
+// var num3 = 50-17
+
+// var num4 = 50+17
+
+
+// document.write(num);
+
+// document.write(num2);
+
+// document.write(num3);
+
+// document.write(num4);
+
+document.getElementById('title').style.color='red';
+document.getElementById('text-chunk').style.background='blue';
+
+
+
+
