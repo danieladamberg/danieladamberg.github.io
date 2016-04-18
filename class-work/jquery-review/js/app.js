e1067599ce7c9eae2d4a9e4923b337161852dde7
danieladamberg <danieladamberg@gmail.com>
2016-04-18 18:38:25 -0400

    Final Project

---
+++ class-work/jquery-review/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready
+
+$('#one').ready(function () {
+	$('#one').css('background', blue);
+}
+
+$('#two').css(background-color='blue');
+
+$('#two').html("this is text");
+
+$('#one').html("this is text");
+
+$('#one').click()
